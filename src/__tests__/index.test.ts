cd7916f7a756d78ac4ec02017a3d1217e5d88447
Minh Cung <minhc@canva.com>
2023-10-18 00:14:30 +1100

    unicode 15.1 - cldr 43 (#146)

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,4 +1,15 @@
-import { type Emoji, getAllEmojis, hasEmoji, compareVersion, stripEmojies, filterEmojis, getAllComponents, extractEmojis, getEmojisByGroup, isValidEmojiVersion } from '../';
+import {
+	type Emoji,
+	getAllEmojis,
+	hasEmoji,
+	compareVersion,
+	stripEmojies,
+	filterEmojis,
+	getAllComponents,
+	extractEmojis,
+	getEmojisByGroup,
+	isValidEmojiVersion,
+} from '../';
 import unicodeEmojis from '../unicode-emojis';
 
 const ztring = 'adfsadfs safdsaf dsafds ';
@@ -8,72 +19,77 @@ const testCases = ['🫶', '👋🏼', '🙏🏿', '👨🏻‍🤝‍👨🏼',
 const testEmojis: Array<Emoji> = testCases.map<Emoji>((e) => ({ emoji: e, version: '1.0', group: 'test' }));
 
 test('hasEmoji', () => {
-    expect(hasEmoji(ztring)).toBe(false);
-    expect(hasEmoji(ztringWithEmoji)).toBe(true);
+	expect(hasEmoji(ztring)).toBe(false);
+	expect(hasEmoji(ztringWithEmoji)).toBe(true);
 });
 
 test('stripEmojis', () => {
-    expect(stripEmojies(ztring)).toBe(ztring);
-    expect(stripEmojies(ztringWithEmoji)).toBe(ztring);
+	expect(stripEmojies(ztring)).toBe(ztring);
+	expect(stripEmojies(ztringWithEmoji)).toBe(ztring);
 });
 
 test('extractEmojis', () => {
-    expect(extractEmojis(ztring)).toEqual([]);
-    expect(extractEmojis(ztringWithEmoji)).toEqual(['👋🏼', '🫶']);
-})
+	expect(extractEmojis(ztring)).toEqual([]);
+	expect(extractEmojis(ztringWithEmoji)).toEqual(['👋🏼', '🫶']);
+});
 
 test('getAllEmojis', () => {
-    expect(getAllEmojis(testEmojis)).toEqual(testCases);
-    expect(getAllEmojis(testEmojis).includes('🧑🏿‍🤝‍🧑🏿')).toBe(false);
+	expect(getAllEmojis(testEmojis)).toEqual(testCases);
+	expect(getAllEmojis(testEmojis).includes('🧑🏿‍🤝‍🧑🏿')).toBe(false);
 
-    const allEmojis = getAllEmojis();
+	const allEmojis = getAllEmojis();
 
-    for (const testCase of testCases) {
-        expect(allEmojis.includes(testCase)).toBe(true);
-    }
+	for (const testCase of testCases) {
+		expect(allEmojis.includes(testCase)).toBe(true);
+	}
 });
 
 test('filterEmojis', () => {
-    expect(filterEmojis('1.0', false, testEmojis)).toEqual(testEmojis);
-    expect(filterEmojis('1.0', true, testEmojis)).toEqual(testEmojis);
-    expect(getAllEmojis(filterEmojis('1.0', true)).includes('😂')).toBe(false); // v0.6
-    expect(getAllEmojis(filterEmojis('1.0', true)).includes('👨🏻‍🤝‍👨🏽')).toBe(false); // v12.1
-    expect(getAllEmojis(filterEmojis('1.0')).includes('😂')).toBe(true);
-    expect(getAllEmojis(filterEmojis('1.0')).includes('👨🏻‍🤝‍👨🏽')).toBe(false);
+	expect(filterEmojis('1.0', false, testEmojis)).toEqual(testEmojis);
+	expect(filterEmojis('1.0', true, testEmojis)).toEqual(testEmojis);
+	expect(getAllEmojis(filterEmojis('1.0', true)).includes('😂')).toBe(false); // v0.6
+	expect(getAllEmojis(filterEmojis('1.0', true)).includes('👨🏻‍🤝‍👨🏽')).toBe(false); // v12.1
+	expect(getAllEmojis(filterEmojis('1.0', true)).includes('🙂‍↔️')).toBe(false); // v15.1 - head shaking
+	expect(getAllEmojis(filterEmojis('1.0')).includes('😂')).toBe(true);
+	expect(getAllEmojis(filterEmojis('1.0')).includes('👨🏻‍🤝‍👨🏽')).toBe(false);
 });
 
 test('getEmojisByGroup', () => {
-    getEmojisByGroup('group', testEmojis).forEach((emojis, key) => expect(emojis.every(emoji => emoji.group === key)).toBe(true));
+	getEmojisByGroup('group', testEmojis).forEach((emojis, key) =>
+		expect(emojis.every((emoji) => emoji.group === key)).toBe(true)
+	);
 
-    const emojisByGroup = getEmojisByGroup('group');
-    emojisByGroup.forEach((emojis, key) => expect(emojis.every(emoji => emoji.group === key)).toBe(true));
+	const emojisByGroup = getEmojisByGroup('group');
+	emojisByGroup.forEach((emojis, key) => expect(emojis.every((emoji) => emoji.group === key)).toBe(true));
 
-    const emojisByCategory = getEmojisByGroup('category');
-    emojisByCategory.forEach((emojis, key) => expect(emojis.every(emoji => emoji.category === key)).toBe(true));
+	const emojisByCategory = getEmojisByGroup('category');
+	emojisByCategory.forEach((emojis, key) => expect(emojis.every((emoji) => emoji.category === key)).toBe(true));
 
-    const emojisBySubgroup = getEmojisByGroup('subgroup');
-    emojisBySubgroup.forEach((emojis, key) => expect(emojis.every(emoji => emoji.subgroup === key)).toBe(true));
-})
+	const emojisBySubgroup = getEmojisByGroup('subgroup');
+	emojisBySubgroup.forEach((emojis, key) => expect(emojis.every((emoji) => emoji.subgroup === key)).toBe(true));
+});
 
 test('getAllComponents', () => {
-    expect(getAllComponents()).toEqual(unicodeEmojis.components);
-})
+	expect(getAllComponents()).toEqual(unicodeEmojis.components);
+});
 
 test('compareVersion', () => {
-    expect(compareVersion('0.6', '0.6')).toBe(true);
-    expect(compareVersion('0.6', '0.6', true)).toBe(true);
-    expect(compareVersion('0.6', '1.0', true)).toBe(false);
-    expect(compareVersion('0.6', '12.0')).toBe(true);
-    expect(compareVersion('12.0', '11.0')).toBe(false);
-})
+	expect(compareVersion('0.6', '0.6')).toBe(true);
+	expect(compareVersion('0.6', '0.6', true)).toBe(true);
+	expect(compareVersion('0.6', '1.0', true)).toBe(false);
+	expect(compareVersion('0.6', '12.0')).toBe(true);
+	expect(compareVersion('12.0', '11.0')).toBe(false);
+	expect(compareVersion('12.0', '15.1')).toBe(true);
+});
 
 test('isValidEmojiVersion', () => {
-    expect(isValidEmojiVersion(null)).toBe(false);
-    expect(isValidEmojiVersion(1)).toBe(false);
-    expect(isValidEmojiVersion("")).toBe(false);
-    expect(isValidEmojiVersion("abc")).toBe(false);
-    expect(isValidEmojiVersion("1.0")).toBe(true);
-    expect(isValidEmojiVersion("2.0")).toBe(true);
-    expect(isValidEmojiVersion("14.0")).toBe(true);
-    expect(isValidEmojiVersion("15.0")).toBe(true);
-})
+	expect(isValidEmojiVersion(null)).toBe(false);
+	expect(isValidEmojiVersion(1)).toBe(false);
+	expect(isValidEmojiVersion('')).toBe(false);
+	expect(isValidEmojiVersion('abc')).toBe(false);
+	expect(isValidEmojiVersion('1.0')).toBe(true);
+	expect(isValidEmojiVersion('2.0')).toBe(true);
+	expect(isValidEmojiVersion('14.0')).toBe(true);
+	expect(isValidEmojiVersion('15.0')).toBe(true);
+	expect(isValidEmojiVersion('15.1')).toBe(true);
+});
--- src/index.ts
+++ src/index.ts
@@ -2,121 +2,124 @@ import emojiRegex from 'emoji-regex';
 import unicodeEmojis from './unicode-emojis';
 
 type Maybe<T> = T | undefined | null;
-export type EmojiVersion = "0.6" |
-    "0.7" |
-    "1.0" |
-    "2.0" |
-    "3.0" |
-    "4.0" |
-    "5.0" |
-    "11.0" |
-    "12.0" |
-    "12.1" |
-    "13.0" |
-    "13.1" |
-    "14.0" |
-    "15.0";
+export type EmojiVersion =
+	| '0.6'
+	| '0.7'
+	| '1.0'
+	| '2.0'
+	| '3.0'
+	| '4.0'
+	| '5.0'
+	| '11.0'
+	| '12.0'
+	| '12.1'
+	| '13.0'
+	| '13.1'
+	| '14.0'
+	| '15.0'
+	| '15.1';
 type CodePoint = string;
 export type Emoji = {
-    emoji: string
-    components?: Maybe<Array<string>>
-    description?: Maybe<string>
-    version: EmojiVersion
-    keywords?: Maybe<Array<string>>
-    codePoint?: Maybe<CodePoint>
-    codePoints?: Maybe<Array<CodePoint>>
-    category?: Maybe<string>
-    group?: Maybe<string>
-    subgroup?: Maybe<string>
-    variations?: Maybe<Array<Emoji>>
-}
+	emoji: string;
+	components?: Maybe<Array<string>>;
+	description?: Maybe<string>;
+	version: EmojiVersion;
+	keywords?: Maybe<Array<string>>;
+	codePoint?: Maybe<CodePoint>;
+	codePoints?: Maybe<Array<CodePoint>>;
+	category?: Maybe<string>;
+	group?: Maybe<string>;
+	subgroup?: Maybe<string>;
+	variations?: Maybe<Array<Emoji>>;
+};
 type Group = 'category' | 'group' | 'subgroup';
 
 /**
  * Check whether a value is a valid Emoji Version
  * @param version - Version
- * @returns 
+ * @returns
  */
 export function isValidEmojiVersion(version: any): version is EmojiVersion {
-    return version === '0.6' ||
-        version === "0.7" ||
-        version === "1.0" ||
-        version === "2.0" ||
-        version === "3.0" ||
-        version === "4.0" ||
-        version === "5.0" ||
-        version === "11.0" ||
-        version === "12.0" ||
-        version === "12.1" ||
-        version === "13.0" ||
-        version === "13.1" ||
-        version === "14.0" ||
-        version === "15.0";
+	return (
+		version === '0.6' ||
+		version === '0.7' ||
+		version === '1.0' ||
+		version === '2.0' ||
+		version === '3.0' ||
+		version === '4.0' ||
+		version === '5.0' ||
+		version === '11.0' ||
+		version === '12.0' ||
+		version === '12.1' ||
+		version === '13.0' ||
+		version === '13.1' ||
+		version === '14.0' ||
+		version === '15.0' ||
+		version === '15.1'
+	);
 }
 
 /**
  * Get All Emojis as an array of emoji character
- * @param emojis 
- * @returns 
+ * @param emojis
+ * @returns
  */
 export function getAllEmojis(emojis?: Array<Emoji>) {
-    return (emojis ?? unicodeEmojis.emojis)
-        .map(e => [e.emoji, ...(e.variations ?? []).map(v => v.emoji)])
-        .flat();
+	return (emojis ?? unicodeEmojis.emojis).map((e) => [e.emoji, ...(e.variations ?? []).map((v) => v.emoji)]).flat();
 }
 
 export function compareVersion(a: EmojiVersion, b: EmojiVersion, exact?: boolean) {
-    if (exact) return parseFloat(a) === parseFloat(b);
-    return parseFloat(a) <= parseFloat(b);
+	if (exact) return parseFloat(a) === parseFloat(b);
+	return parseFloat(a) <= parseFloat(b);
 }
 
 /**
  * Filter Emoijs by the Unicode Version
- * @param version 
- * @param exact 
- * @param emoijs 
- * @returns 
+ * @param version
+ * @param exact
+ * @param emoijs
+ * @returns
  */
 export function filterEmojis(version: EmojiVersion, exact?: boolean, emoijs?: Array<Emoji>) {
-    const filteredEmojis: Array<Emoji> = [];
+	const filteredEmojis: Array<Emoji> = [];
 
-    for (const emoji of (emoijs ?? unicodeEmojis.emojis)) {
-        if (compareVersion(emoji.version, version, exact)) {
-            if (emoji.variations) {
-                emoji.variations = filterEmojis(version, exact, emoji.variations)
-            }
-            filteredEmojis.push(emoji);
-        }
-    }
+	for (const emoji of emoijs ?? unicodeEmojis.emojis) {
+		if (compareVersion(emoji.version, version, exact)) {
+			if (emoji.variations) {
+				emoji.variations = filterEmojis(version, exact, emoji.variations);
+			}
+			filteredEmojis.push(emoji);
+		}
+	}
 
-    return filteredEmojis;
+	return filteredEmojis;
 }
 
 /**
  * Get Emojis by group, category or subgroup
  * @param group category | group | subgroup
- * @param emojis 
- * @returns 
+ * @param emojis
+ * @returns
  */
 export function getEmojisByGroup(group: Group, emojis?: Array<Emoji>) {
-    const map = new Map<string, Array<Emoji>>();
-    for (const emoji of (emojis ?? unicodeEmojis.emojis)) {
-        const groupItem = emoji[group];
-        if (groupItem) {
-            const item = map.get(groupItem) ?? [];
-            item.push(emoji);
-            map.set(groupItem, item);
-        }
-    }
-    return map;
+	const map = new Map<string, Array<Emoji>>();
+	for (const emoji of emojis ?? unicodeEmojis.emojis) {
+		const groupItem = emoji[group];
+		if (groupItem) {
+			const item = map.get(groupItem) ?? [];
+			item.push(emoji);
+			map.set(groupItem, item);
+		}
+	}
+	return map;
 }
 
 /**
  * Get Unicode Emoji Components
- * @returns 
+ * @returns
  */
 export function getAllComponents() {
-    return unicodeEmojis.components;
+	return unicodeEmojis.components;
 }
 
 /**
@@ -124,16 +127,16 @@ export function getAllComponents() {
  * @returns Emoji Regex
  */
 export function getEmojiRegex() {
-    return emojiRegex();
+	return emojiRegex();
 }
 
 /**
  * Check whether a given text has emojis
- * @param text 
+ * @param text
  * @returns text has emojis
  */
 export function hasEmoji(text: string) {
-    return getEmojiRegex().test(text);
+	return getEmojiRegex().test(text);
 }
 
 /**
@@ -150,29 +153,34 @@ export function hasEmoji(text: string) {
  * // => false
  */
 function isString(value: any): value is string {
-    const type = typeof value
-    return type === 'string' || (type === 'object' && value != null && !Array.isArray(value) && Object.prototype.toString.call(value) === '[object String]')
+	const type = typeof value;
+	return (
+		type === 'string' ||
+		(type === 'object' &&
+			value != null &&
+			!Array.isArray(value) &&
+			Object.prototype.toString.call(value) === '[object String]')
+	);
 }
 
 /**
  * Extract Emojis in a given text and preserve the order of appearance
- * @param text 
- * @returns 
+ * @param text
+ * @returns
  */
 export function extractEmojis(text: string) {
-    const emojis = getAllEmojis();
-    return Array.from(text.matchAll(getEmojiRegex()))
-        .map(match => match[0])
-        .map(emojiStr => emojis.find(e => e === emojiStr))
-        .filter<string>(isString);
+	const emojis = getAllEmojis();
+	return Array.from(text.matchAll(getEmojiRegex()))
+		.map((match) => match[0])
+		.map((emojiStr) => emojis.find((e) => e === emojiStr))
+		.filter<string>(isString);
 }
 
 /**
  * Strip / Remove Emojis in a given text
- * @param text 
- * @returns 
+ * @param text
+ * @returns
  */
 export function stripEmojies(text: string) {
-    return text.replace(getEmojiRegex(), '');
+	return text.replace(getEmojiRegex(), '');
 }
-
